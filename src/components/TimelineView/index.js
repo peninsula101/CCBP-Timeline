e43166d13d083dbf55d12ab7f10c0d078b8ccc97
peninsula101 <tvscskiriti@gmail.com>
2023-05-04 07:21:49 +0000

    An approach to develop CCBP Timeline using chrono

---
+++ src/components/TimelineView/index.js
@@ -0,0 +1,51 @@
+import {Chrono} from 'react-chrono'
+
+import ProjectTimelineCard from '../ProjectTimelineCard'
+import CourseTimelineCard from '../CourseTimelineCard'
+
+import {
+  TimelineContainer,
+  ResponsiveContainer,
+  HeaderContainer,
+  Heading,
+  CCBPHeading,
+} from './styledComponents'
+
+const TimelineView = props => {
+  const {timelineItemsList} = props
+
+  const renderTimelineCard = item => {
+    if (item.categoryId === 'PROJECT') {
+      return <ProjectTimelineCard key={item.id} projectDetails={item} />
+    }
+    return <CourseTimelineCard key={item.id} courseDetails={item} />
+  }
+
+  return (
+    <TimelineContainer>
+      <ResponsiveContainer>
+        <HeaderContainer>
+          <Heading>
+            MY JOURNEY IN <br />
+            <CCBPHeading>CCBP 4.0</CCBPHeading>
+          </Heading>
+        </HeaderContainer>
+        <Chrono
+          theme={{
+            primary: 'green',
+            secondary: 'yellow',
+            cardForeColor: 'black',
+            cardBgColor: 'white',
+            titleColor: 'blue',
+          }}
+          items={timelineItemsList}
+          mode="VERTICAL_ALTERNATING"
+        >
+          {timelineItemsList.map(eachItem => renderTimelineCard(eachItem))}
+        </Chrono>
+      </ResponsiveContainer>
+    </TimelineContainer>
+  )
+}
+
+export default TimelineView
